d8f454f4effe07c8cac2af3df0d2d41668fefa6a
fluorjo <fluorjo@naver.com>
2024-04-30 07:01:42 +0900

    page test

---
+++ app/home/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+      <main className="">
+        <div>main</div>
+      </main>
+    );
+  }
+  
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath:"home",
+  output: "export",
+  reactStrictMode: true,
+};
 
 export default nextConfig;
