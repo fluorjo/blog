751ebdd9ec9c8d36f70d938382190d3d2855e8b3
fluorjo <fluorjo@naver.com>
2024-05-07 03:44:48 +0900

    e2e test code

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,5 +1,31 @@
 describe('homepage test', () => {
-  it('home', () => {
-    cy.visit('/')
-  })
-})
+    it('home', () => {
+        cy.visit('/');
+    });
+});
+it('사이드바', () => {
+    cy.get('[data-cy="sidebarToggle"]').click();
+
+    cy.contains('홈');
+    cy.contains('태그');
+
+    cy.get('[data-cy="githubLink"]').should(
+        'have.attr',
+        'href',
+        'https://www.github.com/fluorjo',
+    );
+});
+
+it('글 목록', () => {
+    cy.get('a[href*="/posts/"]').first().click();
+    cy.url().should('include', '/posts/');
+});
+
+it('푸터', () => {
+    // cy.contains('ABOUT ME');
+    // cy.contains('프론트엔드 엔지니어 장동훈');
+    // cy.get('[data-cy="adminLink"]').click();
+    // cy.url().should('include', '/admin');
+    // cy.get('[data-cy="writeLink"]').click();
+    // cy.url().should('not.be.a', '/write');
+});
