f25b330a172b73afadbd8a74aaf37d57c1f8c935
fluorjo <fluorjo@naver.com>
2024-05-11 04:39:32 +0900

    nextrequest 사용해봄.

--- app/api/posts/[id]/route.ts
+++ app/api/posts/[id]/route.ts
@@ -1,8 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { createClient } from '@/utils/supabase/server';
 import { cookies } from 'next/headers';
+import { type NextRequest } from "next/server";
 
 export async function DELETE(
+    _request: NextRequest,
     { params }: { params: { id: number } },
 ) {
     const supabase = await createClient(cookies());
