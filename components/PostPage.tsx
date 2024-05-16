74e3168da729de9794685b74fca28b46d4982d32
fluorjo <fluorjo@naver.com>
2024-05-16 17:28:10 +0900

    postpage에 text component넣기.

--- components/PostPage.tsx
+++ components/PostPage.tsx
@@ -2,10 +2,9 @@ import { MarkdownViewer } from '@/components/Markdown';
 import { Post } from '@/types';
 import { format } from 'date-fns';
 import Image from 'next/image';
-import Link from 'next/link';
 import { FC } from 'react';
 import PostCRUDComponent from './PostCRUD';
-
+import Text from './common/Text';
 const PostPage: FC<Post> = ({
     id,
     title,
@@ -17,27 +16,33 @@ const PostPage: FC<Post> = ({
 }) => {
     return (
         <div className="container flex flex-col gap-8 pb-40 pt-20">
-            <h1 className="text-4xl font-bold">{title}</h1>
+            <Text size="4xl" weight="bold">
+                {title}
+            </Text>
             <div className="flex flex-row items-center gap-2">
-                <Link
-                    href={`/categories/${category}`}
-                    className="rounded-md bg-slate-800 px-2 py-1 text-sm text-white"
+                <Text
+                    link={`/categories/${category}`}
+                    className="rounded-md bg-slate-800 px-2 py-1 "
+                    color="white"
+                    size="sm"
                 >
                     {category}
-                </Link>
+                </Text>
 
                 {tags.map((tag) => (
-                    <Link
-                        href={`/tags/${tag}`}
+                    <Text
+                        link={`/tags/${tag}`}
                         key={tag}
-                        className="rounded-md bg-slate-200 px-2 py-1 text-sm text-slate-500"
+                        className="rounded-md bg-slate-200 px-2 py-1"
+                        color="black"
+                        size="sm"
                     >
                         {tag}
-                    </Link>
+                    </Text>
                 ))}
-                <div className="text-sm text-gray-500">
+                <Text color="grey">
                     {format(new Date(created_at), 'yyyy년 M월 d일 HH:mm')}
-                </div>
+                </Text>
             </div>
             {preview_image_url && (
                 <Image
