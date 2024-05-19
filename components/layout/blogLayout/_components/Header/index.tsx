3f5e09f2a341c95a1f8b20508d613ae65e8e7e2d
fluorjo <fluorjo@naver.com>
2024-05-19 15:58:21 +0900

    검색 버튼 수정

--- components/layout/blogLayout/_components/Header/index.tsx
+++ components/layout/blogLayout/_components/Header/index.tsx
@@ -42,21 +42,30 @@ const Header: FC = () => {
                 </div> */}
             </div>
             <div className="navbar-end">
-                <label className="input input-bordered flex items-center gap-2">
-                    <input type="text" className="grow" placeholder="Search" />
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 16 16"
-                        fill="currentColor"
-                        className="w-4 h-4 opacity-70"
-                    >
-                        <path
-                            fillRule="evenodd"
-                            d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
-                            clipRule="evenodd"
+                <div className="mx-auto max-w-md">
+                    <form action="" className="relative mx-auto w-max">
+                        <input
+                            type="search"
+                            className="peer cursor-pointer relative z-10 h-12 w-12 rounded-full border bg-transparent pl-12 outline-none focus:w-full focus:cursor-text focus:border-gray-500 focus:pl-16 focus:pr-4"
                         />
-                    </svg>
-                </label>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="absolute inset-y-0 my-auto h-8 w-12 border-r border-transparent stroke-gray-500 px-3.5  peer-focus:border-gray-500 peer-focus:stroke-gray-500
+                            
+                            "
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                            strokeWidth="2"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                            />
+                        </svg>
+                    </form>
+                </div>
             </div>
         </div>
     );
