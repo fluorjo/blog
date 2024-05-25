06753970495484a5b2818edecc5ee955a9b329a9
fluorjo <fluorjo@naver.com>
2024-05-25 13:02:00 +0900

    포스트 주소 복사

--- components/PostCRUD.tsx
+++ components/PostCRUD.tsx
@@ -1,8 +1,9 @@
 'use client';
 import Link from 'next/link';
-import { BsPencilFill, BsTrash } from 'react-icons/bs';
+import { BsPencilFill, BsTrash,BsFillShareFill } from 'react-icons/bs';
 import IconButton from './common/IconButton';
 import { useRouter } from 'next/navigation';
+import { FaShareAlt } from "react-icons/fa";
 
 interface PostCRUDComponentProps {
     id: string;
@@ -10,9 +11,22 @@ interface PostCRUDComponentProps {
 
 export default function PostCRUDComponent({ id }: PostCRUDComponentProps) {
     const router = useRouter();
-
+    const copyToClipboard = () => {
+        const url = window.location.href;
+        navigator.clipboard.writeText(url).then(() => {
+            // 모달 등 간단한 알림으로 바꿀 수 없을까. 
+          alert('포스트의 주소가 클립보드에 복사되었습니다.');
+        }).catch((err) => {
+          console.error('클립보드에 주소를 복사하는 동안 오류가 발생했습니다:', err);
+        });
+      };
     return (
         <div className="flex flex-row  gap-x-2">
+            <IconButton
+                Icon={BsFillShareFill}
+                label="editPost"
+                onClick={copyToClipboard}
+            />
             <IconButton
                 Icon={BsPencilFill}
                 component={Link}
